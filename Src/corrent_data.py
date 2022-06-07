c99e0b5413f132de2b9994b2b0a2b38773e9cf45
AndreyRyabov2002 <lelikr239@mail.ru>
2022-06-07 12:44:20 +0300

    First Commin
    First Commin

--- Src/corrent_data.py
+++ Src/corrent_data.py
@@ -1,8 +1,13 @@
 import requests
 
-url = 'http://worldtimeapi.org/api/timezone/Europe/Kirov'
-response = requests.get(url)
+headers = {
+    "User-Agent": "LSKLDS"
+    }
 
-print(response.text,'text')
+#url = 'http://worldtimeapi.org/api/timezone/Europe/Kirov'
+response = requests.get('http://worldtimeapi.org/api/timezone/Europe/Kirov', headers=headers)
+
+print(response.json()['datetime'])
+#print(response.datetime,'datetime')
 #print(response.json(),'json')
 #print(response.content,'content')
