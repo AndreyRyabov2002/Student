6d24dad8554942b3271a2fd2a2d420c0cebaf2da
AndreyRyabov2002 <lelikr239@mail.ru>
2022-06-14 22:14:31 +0300

    First commit
    First commit

--- Src/pythonProject/bot.py
+++ Src/pythonProject/bot.py
@@ -1,16 +1,40 @@
-
+import sqlite3
 import telebot
-import bot_b
+from telethon import TelegramClient, events
+
 bot = telebot.TeleBot("5457646039:AAGJfExqkSzflrYfIjMTsszsJYJquYSeE-E")
+#api_id =14123444
+#api_hash = "ecab1808dbe1520bd8e1f165779ec9ef"
+#client = TelegramClient('bot', api_id, api_hash)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	connect = sqlite3.connect('users_db')
+	cursor = connect.cursor()
+
+	cursor.execute("""CREATE TABLE IF NOT EXISTS login_id(
+		id INTEGER 
+		username TEXT
+	)""")
+
+	connect.commit()
+
+	#check id in fields
+	people_id = message.chat.id
+	cursor.execute(f"SELECT id FROM login_id WHERE id = {people_id}")
+	data = cursor.fetchone()
+	if data is None:
+
+		users_list = [message.chat.id]
+		cursor.execute("INSERT INTO login_id VALUES(?);", users_list)
+		connect.commit()
+	else:
+		bot.send_message(message.chat.id, 'Такой пользователь уже существует.')
 
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Привет")
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+@bot.message_handler(commands=['delete'])
+def delete(message):
+	pass
 
 bot.infinity_polling()
--- Src/pythonProject/id.py
+++ Src/pythonProject/id.py
@@ -1,18 +1,33 @@
-'''from telethon.sync import TelegramClient
+import sqlite3
+import telebot
+
+bot = telebot.TeleBot("5457646039:AAGJfExqkSzflrYfIjMTsszsJYJquYSeE-E")
+
+conn = sqlite3.connect('db/database', check_same_thread=False)
+cursor = conn.cursor()
 
 
-api_id = 14123444
-api_hash = "ecab1808dbe1520bd8e1f165779ec9ef"
-username = "messagestatistics2399bot"
+def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
+	cursor.execute('INSERT INTO users_list (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
+	conn.commit()
 
 
-client = TelegramClient(phone, api_id, api_hash)
-client.connect()
-if not client.is_user_authorized():
-    client.send_code_request(phone)
-    client.sign_in(phone, input('Enter the code: '))
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	bot.send_message(message.chat.id, 'Добро пожаловать')
 
-print('Fetching Members...')
-all_participants = []
-all_participants = client.get_participants(my_chat_id, aggressive=True)
-'''
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+	if message.text.lower() == 'привет':
+		bot.send_message(message.chat.id, 'Привет! Ваше имя добавлено в базу данных!')
+		
+		us_id = message.from_user.id
+		us_name = message.from_user.first_name
+		us_sname = message.from_user.last_name
+		username = message.from_user.username
+		
+		db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
+
+
+bot.infinity_polling()
