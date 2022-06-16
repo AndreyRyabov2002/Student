28e23dc06b8480730f4e54db71cdebe194cdfc5f
AndreyRyabov2002 <lelikr239@mail.ru>
2022-06-16 20:49:51 +0300

    First commit
    First commit

--- Src/pythonProject/bot.py
+++ Src/pythonProject/bot.py
@@ -1,11 +1,11 @@
 import sqlite3
 import telebot
-from telethon import TelegramClient, events
+from telethon import TelegramClient
 
 bot = telebot.TeleBot("5457646039:AAGJfExqkSzflrYfIjMTsszsJYJquYSeE-E")
-#api_id =14123444
-#api_hash = "ecab1808dbe1520bd8e1f165779ec9ef"
-#client = TelegramClient('bot', api_id, api_hash)
+api_id =14123444
+api_hash = "ecab1808dbe1520bd8e1f165779ec9ef"
+client = TelegramClient('bot', api_id, api_hash)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -38,3 +38,12 @@ def delete(message):
 	pass
 
 bot.infinity_polling()
+
+conn = sqlite3.connect("database2.db.db")
+cursor = conn.cursor()
+cursor.execute("""
+               SELECT botik2.username
+               FROM botik2, language, user_language
+               WHERE (user.id = user_language.user_id AND
+               language.id = user_language.language_id)
+                """).fetchall()
--- Src/pythonProject/id.py
+++ Src/pythonProject/id.py
@@ -1,33 +1,45 @@
 import sqlite3
 import telebot
-
+from telethon import TelegramClient
 bot = telebot.TeleBot("5457646039:AAGJfExqkSzflrYfIjMTsszsJYJquYSeE-E")
+api_id =14123444
+api_hash = "ecab1808dbe1520bd8e1f165779ec9ef"
+client = TelegramClient('bot_test', api_id, api_hash)
 
-conn = sqlite3.connect('db/database', check_same_thread=False)
+
+conn = sqlite3.connect('database.db', check_same_thread=False)
 cursor = conn.cursor()
 
 
 def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
-	cursor.execute('INSERT INTO users_list (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
+	cursor.execute('INSERT INTO botik (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)',
+				   (user_id, user_name, user_surname, username))
 	conn.commit()
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id, 'Добро пожаловать')
+	bot.send_message(message.chat.id, 'Напиши "привет"')
+
+
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 	if message.text.lower() == 'привет':
-		bot.send_message(message.chat.id, 'Привет! Ваше имя добавлено в базу данных!')
-		
+		bot.send_message(message.chat.id, 'Что бы посмотреть базу данных, напиши "бд"')
+
 		us_id = message.from_user.id
 		us_name = message.from_user.first_name
 		us_sname = message.from_user.last_name
 		username = message.from_user.username
-		
+
 		db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
+
+	elif message.text == 'бд':
+         file = open('database.db', 'rb')
+         bot.send_document(message.chat.id, file)
+
 
 
 bot.infinity_polling()
